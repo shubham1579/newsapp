0d415594ba11ce3e92e7ab7984131c61d6648c1f
Shubham kumar bharti <79shubham88@gmail.com>
2023-12-06 14:12:08 +0530

    Test commit 8

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -43,7 +43,7 @@ function Favorites() {
                 <div className="mx-auto max-w-2xl px-4 pb-10 sm:px-6 sm:py-0 md:max-w-7xl lg:px-8">
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                         {favoriteArticles.map((ele, i) => (
-                            <div key={i}>
+                            <div key={i} onClick={() => handleClick(ele.article)}>
                                 <div className="group relative border-solid border-2 p-2 border-gray-200">
                                     <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none  lg:h-80 hover:opacity-75 cursor-pointer">
                                         <img
@@ -85,7 +85,7 @@ function Favorites() {
                                     <div className="group relative border-solid border-2 p-2 border-gray-200">
                                         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80 hover:opacity-75 cursor-pointer">
                                             <img
-                                                src={selectedArticle.urlToImage}
+                                                src={selectedArticle.image}
                                                 alt={selectedArticle.title}
                                                 className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                                             />
